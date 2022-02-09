490ffbb0bd92fd04cd67aaf93c420663276ca91f
mytrofanov <mytrofanovmaxym@gmail.com>
2022-02-09 09:11:08 +0200

    currency reducers: date picker

--- src/Pages/Historical.tsx
+++ src/Pages/Historical.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 // @ts-ignore
 import s from './styles/Historical.module.css'
-import CustomDateRangePickerDay from "../Components/Calendar";
+import BasicDatePicker from "../Components/DatePicker";
+
 
 const Historical = () => {
     return (
         <div className={s.historicalPage}>
             <div className={s.calendarBox}>
-                <CustomDateRangePickerDay/>
+            <BasicDatePicker/>
             </div>
         </div>
     );
